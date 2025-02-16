fbd56b02770446c4c3f5901c176bd500e25746d1
Vssppnc <visitsak.phe@gmail.com>
2025-02-16 14:43:46 +0700

    save

---
+++ hw_6.py
@@ -0,0 +1,16 @@
+def mainmenu():
+    print("\n---------Main menu---------")
+    print("1.Insert Data")
+    print("2.Update Data")
+    print("3.Search Data")
+    print("4.Delete Data")
+    print("5.View All Data")
+    print("---------------------------")
+
+def insert_data():
+    pass
+
+def View_alldat():
+    pass
+
+mainmenu()
